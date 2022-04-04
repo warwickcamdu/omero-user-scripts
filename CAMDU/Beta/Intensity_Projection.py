1b733cb20acc5a6f95a54d74d2e1967e9767e7c8
Laura Cooper <l.cooper.5@warwick.ac.uk>
2022-04-04 16:34:50 +0100

    fixed bugs

--- CAMDU/Beta/Intensity_Projection.py
+++ CAMDU/Beta/Intensity_Projection.py
@@ -5,7 +5,6 @@ from omero.gateway import BlitzGateway, DatasetWrapper
 from omero.rtypes import rlong, rstring, robject
 import omero.util.script_utils as script_utils
 import numpy as np
-from time import time
 '''
 Slow, low memory usage
 '''
@@ -88,14 +87,13 @@ def planeGenerator(new_Z, C, T, Z, raw_pixel_store, pixels, projection, shape=No
     """
     for z in range(new_Z):  # createImageFromNumpySeq expects Z, C, T order
         for c in range(C):
-            for t in range(T[0]-1, T[1]):
-                for eachz in range(Z[0]-1, Z[1]):
-                    #plane = pixels.getPlane(eachz, c, t)
-                    plane = get_plane(raw_pixel_store, pixels, 0, 0, 0)
+            for t in range(T[0], T[1]):
+                for eachz in range(Z[0], Z[1]):
+                    plane = get_plane(raw_pixel_store, pixels, eachz, c, t)
                     if shape is not None:
                         plane = plane[shape['y']:shape['y']+shape['h'],
                                       shape['x']:shape['x']+shape['w']]
-                    if eachz == Z[0]-1:
+                    if eachz == Z[0]:
                         new_plane = plane
                     else:
                         if projection == 'Maximum':
@@ -159,87 +157,82 @@ def runScript():
         )
 
     try:
-        for j in range(20):
-            start_time = time()
-            conn = BlitzGateway(client_obj=client)
-            script_params = client.getInputs(unwrap=True)
-            images = getImages(conn, script_params)
-            user = conn.getUser()
+        conn = BlitzGateway(client_obj=client)
+        script_params = client.getInputs(unwrap=True)
+        images = getImages(conn, script_params)
+        user = conn.getUser()
 
-            # Create new dataset if Dataset_Name is defined
+        # Create new dataset if Dataset_Name is defined
+        if "Dataset_Name" in script_params:
+            new_dataset = create_new_dataset(conn,
+                                             script_params["Dataset_Name"])
+
+        for image in images:
+            # If Dataset_Name empty user existing, use new one if not.
             if "Dataset_Name" in script_params:
-                new_dataset = create_new_dataset(conn,
-                                                 script_params["Dataset_Name"])
-
-            for image in images:
-                # If Dataset_Name empty user existing, use new one if not.
-                if "Dataset_Name" in script_params:
-                    dataset = new_dataset
+                dataset = new_dataset
+            else:
+                dataset = image.getParent()
+                if dataset.getOwnerOmeName() != user:
+                    dataset = create_new_dataset(conn, dataset.getName())
+            Z, C, T = image.getSizeZ(), image.getSizeC(), image.getSizeT()
+            if "First_Z" in script_params:
+                Z1 = [script_params["First_Z"], Z]
+            else:
+                Z1 = [0, Z]
+            if "Last_Z" in script_params:
+                Z1[1] = script_params["Last_Z"]
+            if "First_T" in script_params:
+                T1 = [script_params["First_T"], T]
+            else:
+                T1 = [0, T]
+            if "Last_T" in script_params:
+                T1[1] = script_params["Last_T"]
+            # Skip image if Z dimension is 1 or if given Z range is less than 1
+            if (Z != 1) or ((Z1[1]-Z1[0]) >= 1):
+                # Get plane as numpy array
+                raw_pixel_store = conn.c.sf.createRawPixelsStore()
+                query_service = conn.getQueryService()
+                query_string = "select p from Pixels p join fetch p.image i join fetch p.pixelsType pt where i.id='%d'" % int(
+                    script_params["IDs"][0])
+                pixels = query_service.findByQuery(query_string, None)
+                if script_params["Apply_to_ROIs_only"]:
+                    roi_service = conn.getRoiService()
+                    result = roi_service.findByImage(image.getId(), None)
+                    if result is not None:
+                        for roi in result.rois:
+                            for s in roi.copyShapes():
+                                if type(s) == omero.model.RectangleI:
+                                    shape = getRoiShape(s)
+                                    name = "%s_%s_%s" % (image.getName(),
+                                                         s.getId().getValue(),
+                                                         script_params["Method"])
+                                    desc = ("%s intensity Z projection of\
+                                            Image ID: %s, shape ID: %s"
+                                            % (script_params["Method"],
+                                               image.getId(),
+                                               s.getId().getValue()))
                 else:
-                    dataset = image.getParent()
-                    if dataset.getOwnerOmeName() != user:
-                        dataset = create_new_dataset(conn, dataset.getName())
-                Z, C, T = image.getSizeZ(), image.getSizeC(), image.getSizeT()
-                if "First_Z" in script_params:
-                    Z1 = [script_params["First_Z"], Z]
-                else:
-                    Z1 = [1, Z]
-                if "Last_Z" in script_params:
-                    Z1[1] = script_params["Last_Z"]
-                if "First_T" in script_params:
-                    T1 = [script_params["First_T"], T]
-                else:
-                    T1 = [1, T]
-                if "Last_T" in script_params:
-                    T1[1] = script_params["Last_T"]
-                # Skip image if Z dimension is 1 or if given Z range is less than 1
-                if (Z != 1) or ((Z1[1]-Z1[0]) >= 1):
-                    # Get plane as numpy array
-                    raw_pixel_store = conn.c.sf.createRawPixelsStore()
-                    query_service = conn.getQueryService()
-                    query_string = "select p from Pixels p join fetch p.image i join fetch p.pixelsType pt where i.id='%d'" % int(
-                        script_params["IDs"][0])
-                    pixels = query_service.findByQuery(query_string, None)
-                    if script_params["Apply_to_ROIs_only"]:
-                        roi_service = conn.getRoiService()
-                        result = roi_service.findByImage(image.getId(), None)
-                        if result is not None:
-                            for roi in result.rois:
-                                for s in roi.copyShapes():
-                                    if type(s) == omero.model.RectangleI:
-                                        shape = getRoiShape(s)
-                                        name = "%s_%s_%s" % (image.getName(),
-                                                             s.getId().getValue(),
-                                                             script_params["Method"])
-                                        desc = ("%s intensity Z projection of\
-                                                Image ID: %s, shape ID: %s"
-                                                % (script_params["Method"],
-                                                   image.getId(),
-                                                   s.getId().getValue()))
-                    else:
-                        shape = {}
-                        shape['x'] = 0
-                        shape['y'] = 0
-                        shape['w'] = image.getSizeX()
-                        shape['h'] = image.getSizeY()
-                        name = "%s_%s" % (
-                            image.getName(), script_params["Method"])
-                        desc = ("%s intensity Z projection of Image ID: \
-                                 %s" % (script_params["Method"],
-                                        image.getId()))
-                    newImage = conn.createImageFromNumpySeq(
-                        planeGenerator(1, C, T1, Z1, raw_pixel_store, pixels,
-                                       script_params["Method"], shape),
-                        name, 1, C, T1[1]-T1[0], description=desc, dataset=dataset)
-                    copyMetadata(conn, newImage, image)
-                    client.setOutput("New Image", robject(newImage._obj))
-            end_time = time()
-            print("run time:", end_time - start_time)
+                    shape = {}
+                    shape['x'] = 0
+                    shape['y'] = 0
+                    shape['w'] = image.getSizeX()
+                    shape['h'] = image.getSizeY()
+                    name = "%s_%s" % (
+                        image.getName(), script_params["Method"])
+                    desc = ("%s intensity Z projection of Image ID: \
+                             %s" % (script_params["Method"],
+                                    image.getId()))
+                newImage = conn.createImageFromNumpySeq(
+                    planeGenerator(1, C, T1, Z1, raw_pixel_store, pixels,
+                                   script_params["Method"], shape),
+                    name, 1, C, T1[1]-T1[0], description=desc, dataset=dataset)
+                copyMetadata(conn, newImage, image)
+                client.setOutput("New Image", robject(newImage._obj))
 
     finally:
         # Cleanup
         client.closeSession()
-
 
 
 if __name__ == '__main__':
